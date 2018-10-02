67f03a775464a825b88b5e3cef7302a569bc9464
Trần Huy <38495100+huytrv@users.noreply.github.com>
2018-10-02 14:04:28 +0700

    Update BMI.java

--- week4/BMI.java
+++ week4/BMI.java
@@ -1,14 +1,11 @@
 package week4;
 
 public class BMI {
-	public static void evaluateBmi(double weight, double height) {
-		double bmi = weight / (height*height);
-		if (bmi < 18.5) System.out.print("Thiếu cân");
-		if (bmi >= 18.5 & bmi < 23) System.out.print("Bình thường");
-		if (bmi >= 23 & bmi <= 25) System.out.print("Thừa cân");
-		if (bmi > 25) System.out.print("Béo phì");
-	}
-	public static void main(String[] args) {
-		evaluateBmi(40, 1.52);
-	}
+    public static String evaluateBmi(double weight, double height) {
+        double bmi = weight / (height*height);
+        if (bmi < 18.5) return "Thiếu cân";
+        else if (bmi >= 18.5 & bmi < 23) return "Bình thường";
+        else if (bmi >= 23 & bmi <= 25) return "Thừa cân";
+        else return "Béo phì";
+    }
 }
