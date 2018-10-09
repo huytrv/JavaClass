2ccca8320ff98809d18ce979b3831685092a6563
Trần Huy <38495100+huytrv@users.noreply.github.com>
2018-10-09 14:16:24 +0700

    Update Square.java

--- week5/Square.java
+++ week5/Square.java
@@ -10,7 +10,6 @@ public class Square extends Rectangle{
 	}
 	public Square(double side, String color, boolean filled) {
 		super(side, side, color, filled);
-		setSide(side);
 	}
 	public double getSide() {
 		return side;
@@ -18,11 +17,20 @@ public class Square extends Rectangle{
 	public void setSide(double side) {
 		this.side = side;
 	}
+	@Override
+	public void setLength(double length){
+		super.setLength(length);
+		this.side = length;
+	}
+	public void setWidth(double width){
+		super.setWidth(width);
+		this.side = width;
+	}
 	public double getArea() {
 		return side * side;
 	}
 	public double getPerimeter() {
-		return 2 * side;
+		return 4 * side;
 	}
 	public String toString() {
 		return ("Square:\nside: "+ getSide());
